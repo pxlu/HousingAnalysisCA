bf2614b844509cd4ac7a5feb87e2ca5796698029
Peter Lu <peterlu94@gmail.com>
2017-04-04 19:33:15 -0400

    Added a route for specific city data

--- services/routes.py
+++ services/routes.py
@@ -3,10 +3,24 @@ from . import prep_df_for_json, nice_json, dps, data_source
 
 app = Flask(__name__)
 
-@app.route('/city_data')
-def get_all_data():
-  return nice_json(prep_df_for_json(dps.get_specified_regions_data(['Victoria'], data_source)))
+cities_data = prep_df_for_json(dps.get_specified_regions_data(['Victoria', 'Calgary'], data_source))
 
-@app.route('/city_data/<city>')
+# Data for all cities
+@app.route('/data')
+def get_all_data():
+  return nice_json(cities_data)
+
+# Data for a specific city
+@app.route('/data/<city>')
 def get_city_data(city):
+  for key, value in cities_data.items():
+    if key == city:
+      return nice_json(value)
+  error = { "Error" : "No data is available for {}".format(city) }
+  return nice_json(error, 404)
+
+# Data for a specific categories
+@app.route('/data/<category>')
+def get_category_data(category):
+  # TBD, need to do it on the backend script first
   pass
