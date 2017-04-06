59c1bf4c5dd445bbaeaf565a340539fbeba408fe
Peter Lu <peterlu94@gmail.com>
2017-04-06 19:30:43 -0400

    Finished route for region comparison

--- services/comparator.py
+++ services/comparator.py
@@ -1,16 +1,19 @@
 from flask import Flask
-from . import dps, data_source, nice_json, prep_df_for_json, ListConverter
+from . import dps, data_source, nice_json, df_dict_to_json, regions_df_to_json,ListConverter
 
 app = Flask(__name__)
 
-cities_data = prep_df_for_json(dps.get_specified_regions_data('all', data_source))
+cities_data = regions_df_to_json(dps.get_specified_regions_data('all', data_source))
 regions_data = dps.get_specified_regions_data('all', data_source)
 app.url_map.converters['list'] = ListConverter
 
 @app.route('/compare/<list:cities_to_compare>')
 def compare_cities(cities_to_compare):
+
   cities = []
   for city in cities_to_compare:
     cities.append(city)
 
-  return nice_json(cities)
+  compared_regions_data = dps.compare_on_regions(regions_data, None, None, None, cities=cities)
+
+  return nice_json(df_dict_to_json(compared_regions_data))
