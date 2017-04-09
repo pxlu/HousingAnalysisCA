7a5f9e2248d078f1c51da4e12f555afd02235458
Peter Lu <peterlu94@gmail.com>
2017-04-09 14:55:50 -0400

    Moved functions from __init__.py to comparator.py, and reformatted routes to work with new logic

--- services/comparator.py
+++ services/comparator.py
@@ -1,27 +1,36 @@
 from flask import Flask
-from . import dps, data_source, nice_json, df_dict_to_json, regions_df_to_json, evaluate_params, ListConverter
+from . import dps, data_source, nice_json, df_dict_to_json, regions_df_to_json,ListConverter
 
 app = Flask(__name__)
 
 regions_data = dps.get_specified_regions_data('all', data_source)
 app.url_map.converters['list'] = ListConverter
 
+def evaluate_params(params):
+
+  parameters = {str(param.split("=")[0]) : str(param.split("=")[1]) for param in params}
+  for key, value in parameters.items():
+    parameters[key] = value.split('+')
+
+  return parameters
+
+def compare(params, compare_on):
+
+  # start_date='Jan 2005', time_interval=12, interval_type='monthly'
+
+  parameters = evaluate_params(params)
+  out = dps.compare_on(compare_on, regions_data, parameters[compare_on])
+
+  return nice_json(df_dict_to_json(out))
+
 @app.route('/compare/cities/<list:params>')
 def compare_cities(params):
 
-  parameters = evaluate_params(params)
-
-  out = dps.compare_on_regions(regions_data, None, None, None, cities=parameters['cities'])
-
-  return nice_json(df_dict_to_json(out))
+  return compare(params, 'cities')
 
 @app.route('/compare/categories/<list:params>')
 def compare_categories(params):
 
-  parameters = evaluate_params(params)
-
-  out = dps.compare_on_categories(regions_data, attributes=parameters['categories'])
-
-  return nice_json(df_dict_to_json(out))
+  return compare(params, 'categories')
 
 
