5d56d2d9d9423fad6e0f83932dbb008b9115eaaf
Peter Lu <peterlu94@gmail.com>
2017-05-02 16:47:10 -0400

    Finished one_vs_all_cities

--- scripts/insights.py
+++ scripts/insights.py
@@ -1,7 +1,5 @@
 from __future__ import division
 import json
-import pprint
-import os
 
 # Things to implement:
   # Describes average increase of parameter over the data for the time interval given
@@ -9,7 +7,12 @@ import os
     # One vs One -- For cities/categories
     # One vs All -- For cities/categories
 
-def calculate_ratio(name, city_1, city_2, city_1_value, city_2_value, date=None):
+def load_json(json_name):
+  with open(json_name, "r") as json_file:
+    data = json.load(json_file)
+  return data
+
+def calculate_ratio_one_date(name, city_1, city_2, city_1_value, city_2_value, date=None):
 
   reverse = False
   if not date:
@@ -22,27 +25,45 @@ def calculate_ratio(name, city_1, city_2, city_1_value, city_2_value, date=None)
     reverse = True
   ratio = larger_value / smaller_value
   if not reverse: 
-    return "On {}, {}'s {} is {}% greater than {}'s".format(date, city_1, name, round((ratio-1)*100, 2), city_2)
+    return "{} -- {}'s {} is {}% greater than {}'s".format(date, city_1, name, round((ratio-1)*100, 2), city_2)
   else:
-    return "On {}, {}'s {} is {}% greater than {}'s".format(date, city_2, name, round((ratio-1)*100, 2), city_1)
+    return "{} -- {}'s {} is {}% greater than {}'s".format(date, city_2, name, round((ratio-1)*100, 2), city_1)
 
-def one_vs_one_cities(json_name, city_1, city_2, compare_on):
+def calculate_ratio_multi_dates(name, city_1, city_2, c1_values, c2_values, dates=[]):
 
-  with open(json_name, "r") as json_file:
-    data = json.load(json_file)
+  msgs = [calculate_ratio_one_date(name, city_1, city_2, c1_values, c2_values, date) for date in dates]
+
+  return msgs
+
+def one_vs_one_cities(data, city_1, city_2, compare_on, dates):
+
   shared_categories = list(set([str(key) for key in data[city_1].keys()]) & set([str(key) for key in data[city_2].keys()]))
 
   for key in compare_on:
     if key not in shared_categories:
       continue
     # Over one date?
-    ratio_quote = calculate_ratio(key, city_1, city_2, data[city_1][key], data[city_2][key], 'Apr 2005')
-    print ratio_quote
+    if len(dates) == 1:
+      ratio_quote = calculate_ratio_one_date(key, city_1, city_2, data[city_1][key], data[city_2][key], dates[0])
+      return ratio_quote
+    else:
     # Over multiple dates ...
-    pass
+      ratio_quotes = calculate_ratio_multi_dates(key, city_1, city_2, data[city_1][key], data[city_2][key], dates)
+      return ratio_quotes
+
+# ===== #
+
+def one_vs_all_cities(json_name, city, compare_on, dates):
+
+  data = load_json(json_name)
+  out = {city + " vs " + city_name : one_vs_one_cities(data, city, city_name, compare_on, dates) for city_name, city_data in data.items() if city_name != city}
+
+  return out
 
 def main():
-  one_vs_one_cities("../test-data/cal_vic.json", "Calgary", "Victoria", ['One_Storey_Benchmark', 'Apartment_Benchmark'])
+  # one_vs_one_cities("../test-data/cal_vic.json", "Calgary", "Victoria", ['One_Storey_Benchmark', 'Apartment_Benchmark'], ['Apr 2005', 'May 2005', 'Jun 2005'])
+  out = one_vs_all_cities("../test-data/cal_vic.json", "Calgary", ['One_Storey_Benchmark', 'Apartment_Benchmark'], ['Apr 2005', 'May 2005', 'Jun 2005'])
+  print out
 
 if __name__=='__main__':
   main()
