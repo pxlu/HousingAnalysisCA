ca2db8713cf5955f77f97cd0c3348fe64bff1c37
Peter Lu <peterlu94@gmail.com>
2017-05-08 17:11:10 -0400

    Converted both forms of ratio calculations to one generic one

--- scripts/insights.py
+++ scripts/insights.py
@@ -13,27 +13,32 @@ def load_json(json_name):
     data = json.load(json_file)
   return data
 
-def calculate_ratio_one_date(ratio_type, name, city_1, city_2, city_1_value, city_2_value, date=None):
+def calculate_ratio_one_date(compare_type, compare_on, comparator_1, comparator_2, c1_value, c2_value, date=None):
 
   reverse = False
   if not date:
     return False
-  c1_v, c2_v = city_1_value[date], city_2_value[date]
+  c1_v, c2_v = c1_value[date], c2_value[date]
   if c1_v > c2_v:
     larger_value,smaller_value = c1_v,c2_v
   else:
     larger_value,smaller_value = c2_v,c1_v
     reverse = True
   ratio = larger_value / smaller_value
-  if ratio_type == 'cities':
+  if compare_type == 'categories':
     if not reverse: 
-      return "{} -- {}'s {} is {}% greater than {}'s".format(date, city_1, name, round((ratio-1)*100, 2), city_2)
+      return "{} -- {}'s {} is {}% greater than its {}".format(date, compare_on, comparator_1, round((ratio-1)*100, 2), comparator_2)
     else:
-      return "{} -- {}'s {} is {}% greater than {}'s".format(date, city_2, name, round((ratio-1)*100, 2), city_1)
+      return "{} -- {}'s {} is {}% greater than its {}".format(date, compare_on, comparator_2, round((ratio-1)*100, 2), comparator_1)
+  else:
+    if not reverse: 
+      return "{} -- {}'s {} is {}% greater than {}'s".format(date, comparator_1, compare_on, round((ratio-1)*100, 2), comparator_2)
+    else:
+      return "{} -- {}'s {} is {}% greater than {}'s".format(date, comparator_2, compare_on, round((ratio-1)*100, 2), comparator_1)
 
-def calculate_ratio_multi_dates(ratio_type, name, city_1, city_2, c1_values, c2_values, dates=[]):
+def calculate_ratio_multi_dates(compare_type, compare_on, comparator_1, comparator_2, c1_value, c2_value, dates=[]):
 
-  msgs = [calculate_ratio_one_date(ratio_type, name, city_1, city_2, c1_values, c2_values, date) for date in dates]
+  msgs = [xcalculate_ratio_one_date(compare_type, compare_on, comparator_1, comparator_2, c1_value, c2_value, date) for date in dates]
 
   return msgs
 
@@ -46,9 +51,7 @@ def one_vs_all_cities(json_name, city, compare_on, dates):
 
   return out
 
-def convert_to_categories_dict(json_name):
-
-  cities_dict = load_json(json_name)
+def convert_to_categories_dict(cities_dict):
 
   out = {}
   for city, data in cities_dict.items():
@@ -60,48 +63,35 @@ def convert_to_categories_dict(json_name):
 
   return out
 
-def one_vs_one_categories(ratio_type, data, category_1, category_2, cities, dates):
-
-  shared_cities = list(set([str(key) for key in data[category_1].keys()]) & set([str(key) for key in data[category_2].keys()]))
-
-  for key in cities:
-    if key not in shared_cities:
-      continue
-    # Over one date?
-    if len(dates) == 1:
-      ratio_quote = calculate_ratio_one_date(ratio_type, key, category_1, category_2, data[category_1][key], data[category_2][key], dates[0])
-      return ratio_quote
-    else:
-    # Over multiple dates ...
-      ratio_quotes = calculate_ratio_multi_dates(ratio_type, key, category_1, category_2, data[category_1][key], data[category_2][key], dates)
-      return ratio_quotes
-
 def one_vs_one_comparison(compare_type, data, comparator_1, comparator_2, compare_on, dates):
 
   shared_attributes = list(set([str(key) for key in data[comparator_1].keys()]) & set([str(key) for key in data[comparator_2].keys()]))
 
+  out = []
   for key in compare_on:
     if key not in shared_attributes:
       continue
     # Over one date?
     if len(dates) == 1:
       ratio_quote = calculate_ratio_one_date(compare_type, key, comparator_1, comparator_2, data[comparator_1][key], data[comparator_2][key], dates[0])
-      return ratio_quote
+      out.append(ratio_quote)
     else:
     # Over multiple dates ...
       ratio_quotes = calculate_ratio_multi_dates(compare_type, key, comparator_1, comparator_2, data[comparator_1][key], data[comparator_2][key], dates)
-      return ratio_quotes
+      out.append(ratio_quotes)
 
-  # Compare between category 1 and 2 for each city in cities
-  # Need to convert data into an dictionary indexed by categories as opposed to cities first
+  return out
 
 def main():
   data = load_json("../test-data/cal_vic.json")
-  out = one_vs_all_cities("../test-data/cal_vic.json", "Calgary", ['One_Storey_Benchmark', 'Apartment_Benchmark'], ['Apr 2005', 'May 2005', 'Jun 2005'])
-  # print out
-  out2 = one_vs_one_categories(convert_to_categories_dict("../test-data/cal_vic.json"), 'Apartment_Benchmark', 'Benchmark Average', ['Calgary'], ['Apr 2005', 'May 2005', 'Jun 2005'])
-  # print out2
-  print one_vs_one_comparison('cities', data, "Calgary", "Victoria", ['One_Storey_Benchmark', 'Apartment_Benchmark'], ['Apr 2005', 'May 2005', 'Jun 2005'])
+  """
+  out = one_vs_one_comparison('cities', data, "Calgary", "Victoria", ['One_Storey_Benchmark', 'Apartment_Benchmark'], ['Apr 2005', 'May 2005', 'Jun 2005'])
+  print out
+  print one_vs_one_comparison('categories', convert_to_categories_dict(data), 'Apartment_Benchmark', 'Benchmark Average', ['Calgary'], ['Apr 2005', 'May 2005', 'Jun 2005'])
+  """
+  out = one_vs_one_comparison('cities', data, "Calgary", "Victoria", ['One_Storey_Benchmark', 'Apartment_Benchmark'], ['Apr 2005', 'May 2005', 'Jun 2005'])
+  print out
+  print one_vs_one_comparison('categories', convert_to_categories_dict(data), 'Apartment_Benchmark', 'Benchmark Average', ['Calgary'], ['Apr 2005', 'May 2005', 'Jun 2005'])
   # NEED TO IMPLEMENT PRINT RATIO FUNCTION FOR CATEGORIES
 
 if __name__=='__main__':
